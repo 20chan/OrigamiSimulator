4a4639e3b3f03d103989f2da30e7971d2939e6e4
20chan <2@0chan.dev>
2019-09-18 00:37:48 +0900

    face 접힌쪽 판정 및 뒤집기 구현
    intersection 이 모서리의 끝부분일 경우 인덱스가 엉망이 되서 계산이 틀어지는 버그가 있다

--- Origami/Paper.Face.cs
+++ Origami/Paper.Face.cs
@@ -17,24 +17,22 @@ namespace Origami {
 
             public bool TryFold(Line mark, out Face newFace) {
                 // TODO: face 전체가 접히는 경우는 어떡하지?
-                // 파라미터로 이쪽으로 접는거야 하고 방향을 알려줘야 한다
-
-                // 모서리들과 폴드된 부분은 짝수이다 (예외는 일단 제외하고 생각하자)
-                // 첫번째 접점과 두번쨰 접점을 잇고 사이의 모든 vertices 는 markLine 에 반전시켜
-                // 새로운 Face로 만들어 리턴하면 된다
-
-                // todo: mark 만으로는 방향을 알 수 없다. 방향을 알아야 함
-                // todo: newFace를 markLine 에 대해 대칭이동 시켜야한다
 
                 var verts = new CyclicArray<Vector>(_vertices.ToArray());
                 var intersects = new List<Vector>();
                 var indices = new List<int>();
+                Vector? prevIntersect = null;
 
                 for (var i = 0; i < verts.Length; i++) {
                     var prev = verts[i - 1];
                     var cur = verts[i];
                     var side = new LineSegment(prev, cur);
                     if (mark.TryGetIntersects(side, out var ints)) {
+                        if (prevIntersect.HasValue && prevIntersect.Value.Equals(ints[0])) {
+                            indices[indices.Count - 1] = i;
+                            continue;
+                        }
+                        prevIntersect = ints[0];
                         intersects.Add(ints[0]);
                         indices.Add(i);
                     }
@@ -44,23 +42,38 @@ namespace Origami {
                     return false;
                 }
 
-                _vertices = new List<Vector>();
-                _vertices.Add(intersects[1]);
-                _vertices.Add(intersects[0]);
+                var v0 = new List<Vector> {
+                    intersects[1],
+                    intersects[0]
+                };
 
                 for (var i = indices[0]; i != indices[1]; i = (i + 1) % verts.Length) {
-                    _vertices.Add(verts[i]);
+                    v0.Add(verts[i]);
                 }
 
-                var newVerts = new List<Vector>();
-                newVerts.Add(intersects[0]);
-                newVerts.Add(intersects[1]);
+                var v1 = new List<Vector> {
+                    intersects[0],
+                    intersects[1]
+                };
 
                 for (var i = indices[1]; i != indices[0]; i = (i + 1) % verts.Length) {
-                    newVerts.Add(verts[i]);
+                    v1.Add(verts[i]);
                 }
 
-                newFace = new Face(newVerts);
+                // v0 가 새로운 face의 verticies 인지
+                // 이를 통해 무조건 v0를 현재 face의 verticies로 만든다
+                if (mark.GetSideOf(v0[2]) < 0) {
+                    var temp = v0;
+                    v0 = v1;
+                    v1 = temp;
+                }
+
+                for (int i = 2; i < v1.Count; i++) {
+                    v1[i] = mark.GetPerpendicular(v1[i]);
+                }
+
+                _vertices = v0;
+                newFace = new Face(v1);
                 return true;
             }
         }
